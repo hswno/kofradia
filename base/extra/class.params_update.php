c82976c35ea00b45ae86d0ef58d3b203d4014468
Henrik Steen <henrist@henrist.net>
2013-03-08 00:31:42 +0100

    La til variabel i params_update for å holde styr på endring-status. For å kunne frigjøre uten commit hvis databasen allerede er frigjort.

--- base/extra/class.params_update.php
+++ base/extra/class.params_update.php
@@ -17,6 +17,9 @@ class params_update extends params
 	
 	/** State (locked) */
 	private $locked = false;
+
+	/** Endret siden forrige lagring? */
+	private $changed_state = false;
 	
 	/** Constructor - Link mot en bestemt celle i en tabell */
 	public function __construct($text, $table, $column, $where)
@@ -71,6 +74,7 @@ class params_update extends params
 		if (isset($this->params[$name]))
 		{
 			unset($this->params[$name]);
+			$this->changed_state = true;
 			$ret = true;
 		}
 		
@@ -79,7 +83,7 @@ class params_update extends params
 		{
 			$this->commit();
 		}
-		
+
 		return $ret;
 	}
 	
@@ -94,6 +98,7 @@ class params_update extends params
 		
 		// oppdater params
 		$this->params[$name] = $value;
+		$this->changed_state = true;
 		
 		// lagre?
 		if ($save)
@@ -110,9 +115,19 @@ class params_update extends params
 		{
 			throw new HSException("params_update->commit: Cannot commit unlocked row.");
 		}
+
+		// ingen endringer?
+		if (!$this->changed_state) {
+			if ($free) {
+				if (ess::$b->db->transaction) ess::$b->db->commit();
+				$this->locked = false;
+			}
+			return;
+		}
 		
 		// oppdater databasen
 		ess::$b->db->query("UPDATE $this->link_table SET $this->link_column = ".ess::$b->db->quote($this->build())." WHERE $this->link_where LIMIT 1");
+		$this->changed_state = false;
 		
 		// frigjøre?
 		if ($free)
