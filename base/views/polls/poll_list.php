6aebf85d5015d70825f0b793a247725bc5c2b12e
Henrik Steen <henrist@henrist.net>
2013-12-24 09:03:24 +0100

    Rettet på feil i avstemminger.

--- base/views/polls/poll_list.php
+++ base/views/polls/poll_list.php
@@ -14,7 +14,7 @@ else
 	foreach ($polls as $poll)
 	{
 		// har vi stemt?
-		$vote = $poll->getVote();
+		$vote = login::$logged_in ? $poll->getVote() : null;
 
 		echo \Kofradia\View::forge("polls/poll_item", array(
 			"poll" => $poll,
@@ -25,7 +25,7 @@ else
 if ($pagei->pages > 1)
 {
 	echo '
-<p class="c">'.$pagei->pagenumbers('polls', 'polls/_pageid_').'</p>';
+<p class="c">'.$pagei->pagenumbers('/polls', '/polls/_pageid_').'</p>';
 }
 
 if (access::has("forum_mod"))
