1b97731288315f685ab8fcc45f86041ccb45ea3f
Henrik Steen <henrist@henrist.net>
2013-12-27 02:34:47 +0100

    Endret iframe til webhat til å bruke https i stedet for http, slik at denne blir synlig i f.eks. Google Chrome.

--- public/irc/client/index.php
+++ public/irc/client/index.php
@@ -17,7 +17,7 @@ echo '
 <p>
 	Hvis du ikke ønsker å laste ned og installere mIRC på din egen PC kan du bruke dette som et alternativ for å prate på chatten!
 </p>
-<iframe src="http://webchat.quakenet.org/?nick='.urlencode($ircnick."[KF]").'&channels=kofradia&uio=OT10cnVlJjExPTE5NQ64" width="100%" height="400"></iframe>';
+<iframe src="https://webchat.quakenet.org/?nick='.urlencode($ircnick."[KF]").'&channels=kofradia&uio=OT10cnVlJjExPTE5NQ64" width="100%" height="400"></iframe>';
 
 ess::$b->page->add_body_post('
 <script type="text/javascript">
