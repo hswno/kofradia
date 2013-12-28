66db64d87121be7dadb8a4605e643732e4a818be
Henrik Steen <henrist@henrist.net>
2013-12-28 05:29:26 +0100

    Legger til composer.lock slik at produksjonserveren kun bruker testede pakker.

--- pull_all.sh
+++ pull_all.sh
@@ -19,6 +19,16 @@ do
 	cd $c
 done
 
+# Vi kjører 'composer install' i stedet for 'composer update',
+# slik at den installerer pakkene spesifisert i composer.lock
+#
+# Hvis pakker skal oppdateres til nyere versjoner skal
+# 'composer update' kjøres på utviklerserver, testes og så
+# depoyes ved å commite ny composer.lock og kjøre dette scriptet
+# på produksjonsserveren
+#
+# Se også http://adamcod.es/2013/03/07/composer-install-vs-composer-update.html
+
 cd kofradia
 composer install --no-dev --optimize-autoloader
 cd ..
