1800be0ef7d831d61e2861190583529649690856
daniehol <daniehol@ifi.uio.no>
2015-12-01 00:12:53 +0100

    Vi har visst kommet til aar 2015

--- app/pages/julekalender.php
+++ app/pages/julekalender.php
@@ -191,7 +191,7 @@ class page_julekalender extends pages_player
 
 		$d = $this->get_today(true);
 		$today = $d->format("j");
-		$date_ok = $d->format("Y-m") == "2013-12";
+		$date_ok = $d->format("Y-m") == "2015-12";
 
 		$data = $this->obj->load_data();
 		$my_answers = $this->obj->load_my_answers();
